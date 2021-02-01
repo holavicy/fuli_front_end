029f33cef2dde51dfc7a35343bacce2a0bec9b65
cmj <819953059@qq.com>
2021-02-01 10:10:55 +0800

    提交

--- src/api/service.js
+++ src/api/service.js
@@ -82,7 +82,7 @@ function createRequestFunction (service) {
         Authorization: token,
         'Content-Type': get(config, 'headers.Content-Type', 'application/json')
       },
-      timeout: 20000,
+      timeout: 100000,
       baseURL: process.env.VUE_APP_API,
       data: {}
     }
